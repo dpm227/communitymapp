919b6b4d19c09d5c7e62083e3c15f6f0ec59a655
Linda Geer <lindageer@Lindas-MacBook-Air-2.local>
2023-11-11 17:58:22 -0500

    reformatted to work with flask again

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, request, flash
 # initialize app
 app = Flask(__name__)
 
-app.secret_key = 'secret_key'
-
 # renders the default page
 @app.route("/")
 def index():
     return render_template("index.html")
+
+# renders the default page
+@app.route("/live")
+def live():
+    return render_template("live.html")
