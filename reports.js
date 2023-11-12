4f94b992413ca75aa2c4121019dc35c31cf91b52
James Chang <jameschang2005@icloud.com>
2023-11-11 23:19:20 -0500

    l

--- reports.js
+++ reports.js
@@ -1,2 +1,7 @@
-let word = "x"
-document.getElementById('reports').innerHTML += ('<li class = "center">'+word+'</li>');
+const para = document.createElement("p");
+const node = document.createTextNode("This is new.");
+para.appendChild(node);
+
+const element = document.getElementById("div1");
+const child = document.getElementById("p1");
+element.insertBefore(para,child);
