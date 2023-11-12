2b36f29c5fa1cbd137a6b1c6a798ca72f674e0f9
James Chang <jameschang2005@icloud.com>
2023-11-11 23:40:29 -0500

    semi-working report.js file

--- reports.js
+++ reports.js
@@ -1,7 +1,17 @@
-const para = document.createElement("p");
-const node = document.createTextNode("This is new.");
-para.appendChild(node);
+(async () => {
+    const res = await fetch('https://icanhazdadjoke.com/', {
+      headers: { Accept: 'application/json' },
+    });
+    const json = await res.json();
+    Object.entries(json).forEach(([key, value]) => {
 
-const element = document.getElementById("div1");
-const child = document.getElementById("p1");
-element.insertBefore(para,child);
+        const para = document.createElement("p");
+        const node = document.createTextNode(`${key}: ${value}`);
+        para.appendChild(node);
+
+        const element = document.getElementById("div1");
+        const child = document.getElementById("p1");
+        element.insertBefore(para,child);
+
+    });
+  })();
